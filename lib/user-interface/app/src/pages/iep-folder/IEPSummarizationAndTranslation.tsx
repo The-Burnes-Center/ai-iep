2aea0c644158c9e63cd6887e8dabf1634055b5f1
ayushkhanvilkar10 <khanvilkar.a@northeastern.edu>
2025-07-27 18:40:58 -0400

    feat: translations for rights shown while document is processing

--- lib/user-interface/app/src/pages/iep-folder/IEPSummarizationAndTranslation.tsx
+++ lib/user-interface/app/src/pages/iep-folder/IEPSummarizationAndTranslation.tsx
@@ -115,45 +115,49 @@ const IEPSummarizationAndTranslation: React.FC = () => {
     }
   };
 
-  // Parent Rights carousel data - same as ParentRights.tsx
-  const parentRightsSlideData = [
-    {
-      id: 'slide-1',
-      title: 'You can request a translator',
-      content: 'You can request a translator for IEP meetings to ensure clear communication.',
-      image: '/images/carousel/surprised.png'
-    },
-    {
-      id: 'slide-2',
-      title: 'You can take your time',
-      content: 'You have the right to take your time before signing an IEP - you don\'t need to sign until you\'re ready.',
-      image: '/images/carousel/blissful.png'
-    },
-    {
-      id: 'slide-3',
-      title: 'You can consent or not',
-      content: 'You can consent to all, some, or none of the proposed services - your child won\'t receive new services without your approval.',
-      image: '/images/carousel/joyful.png'
-    },
-    {
-      id: 'slide-4',
-      title: 'You can request a meeting',
-      content: 'You have the right to request an IEP meeting at any time, not just at  the annual review, and the school must schedule it within 30 days.',
-      image: '/images/carousel/surprised.png'
-    },
-    {
-      id: 'slide-5',
-      title: 'You can reschedule',
-      content: 'If an administrator isn\'t present at the meeting, you have the right to reschedule for a time when they can attend.',
-      image: '/images/carousel/blissful.png'
-    },
-    {
-      id: 'slide-6',
-      title: 'You must be given a booklet of your rights',
-      content: 'By law, your case manager must provide you with a booklet of your parental rights before the IEP meeting.',
-      image: '/images/carousel/confident.png'
-    }
-  ];
+  // Parent Rights carousel data - internationalized using useLanguage hook
+  const parentRightsSlideData = useMemo(() => {
+    if (!translationsLoaded) return [];
+    
+    return [
+      {
+        id: 'slide-1',
+        title: t('rights.slide1.title'),
+        content: t('rights.slide1.content'),
+        image: '/images/carousel/surprised.png'
+      },
+      {
+        id: 'slide-2',
+        title: t('rights.slide2.title'),
+        content: t('rights.slide2.content'),
+        image: '/images/carousel/blissful.png'
+      },
+      {
+        id: 'slide-3',
+        title: t('rights.slide3.title'),
+        content: t('rights.slide3.content'),
+        image: '/images/carousel/joyful.png'
+      },
+      {
+        id: 'slide-4',
+        title: t('rights.slide4.title'),
+        content: t('rights.slide4.content'),
+        image: '/images/carousel/surprised.png'
+      },
+      {
+        id: 'slide-5',
+        title: t('rights.slide5.title'),
+        content: t('rights.slide5.content'),
+        image: '/images/carousel/blissful.png'
+      },
+      {
+        id: 'slide-6',
+        title: t('rights.slide6.title'),
+        content: t('rights.slide6.content'),
+        image: '/images/carousel/confident.png'
+      }
+    ];
+  }, [t, translationsLoaded]);
 
   // Handle jargon click
   const handleContentClick = (e: React.MouseEvent) => {
