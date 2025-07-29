338fb15b867f7cd3398308e3c670be19dc09d7a5
ayushkhanvilkar10 <khanvilkar.a@northeastern.edu>
2025-07-28 23:44:49 -0400

    feat: translations for the tutorial slides

--- lib/user-interface/app/src/pages/iep-folder/IEPSummarizationAndTranslation.tsx
+++ lib/user-interface/app/src/pages/iep-folder/IEPSummarizationAndTranslation.tsx
@@ -129,45 +129,7 @@ const IEPSummarizationAndTranslation: React.FC = () => {
     setActiveTab(lang);
   };
 
-  // AppTutorial carousel data
-  const appTutorialSlideData = [
-    {
-      id: 'slide-1',
-      title: 'What are we doing right now?',
-      content: 'We\'re summarizing the key aspects of your IEP. You\'ll first see a general overview of the document, and then a summary for each one of the key sections.',
-      image: '/images/carousel/surprised.png'
-    },
-    {
-      id: 'slide-2',
-      title: 'What are we doing right now?',
-      content: 'We\'re including the pages where we found the information we\'re about to show. That way, you can double check the information in case something doesn\'t make sense.',
-      image: '/images/carousel/blissful.png'
-    },
-    {
-      id: 'slide-3',
-      title: 'What are we doing right now?',
-      content: 'We\'re translating the summaries to your language. The IEP document will remain in English, but the summaries we created will be translated to the language of your choice.',
-      image: '/images/carousel/joyful.png'
-    },
-    {
-      id: 'slide-4',
-      title: 'What are we doing right now?',
-      content: 'We\'re removing your personal information from the summaries. You can rest assured that we will not store any of your or your child\'s personal details.',
-      image: '/images/carousel/surprised.png'
-    },
-    {
-      id: 'slide-5',
-      title: 'What are we doing right now?',
-      content: 'Your IEP document won\'t be changed. You can download the summaries we\'re creating by clicking on the download button.',
-      image: '/images/carousel/blissful.png'
-    },
-    {
-      id: 'slide-6',
-      title: 'What are we doing right now?',
-      content: 'We\'re creating a glossary to help you understand the document. You can click over the highlighted words and read their definitions on the panel that will emerge.',
-      image: '/images/carousel/confident.png'
-    }
-  ];
+
 
   // Tutorial flow functions
   const handleSkipToRights = () => {
@@ -217,6 +179,50 @@ const IEPSummarizationAndTranslation: React.FC = () => {
         id: 'slide-6',
         title: t('rights.slide6.title'),
         content: t('rights.slide6.content'),
+        image: '/images/carousel/confident.png'
+      }
+    ];
+  }, [t, translationsLoaded]);
+
+      // AppTutorial carousel data - internationalized using useLanguage hook
+  const appTutorialSlideData = useMemo(() => {
+    if (!translationsLoaded) return [];
+    
+    return [
+      {
+        id: 'slide-1',
+        title: t('appTutorial.slide1.title'),
+        content: t('appTutorial.slide1.content'),
+        image: '/images/carousel/surprised.png'
+      },
+      {
+        id: 'slide-2',
+        title: t('appTutorial.slide2.title'),
+        content: t('appTutorial.slide2.content'),
+        image: '/images/carousel/blissful.png'
+      },
+      {
+        id: 'slide-3',
+        title: t('appTutorial.slide3.title'),
+        content: t('appTutorial.slide3.content'),
+        image: '/images/carousel/joyful.png'
+      },
+      {
+        id: 'slide-4',
+        title: t('appTutorial.slide4.title'),
+        content: t('appTutorial.slide4.content'),
+        image: '/images/carousel/surprised.png'
+      },
+      {
+        id: 'slide-5',
+        title: t('appTutorial.slide5.title'),
+        content: t('appTutorial.slide5.content'),
+        image: '/images/carousel/blissful.png'
+      },
+      {
+        id: 'slide-6',
+        title: t('appTutorial.slide6.title'),
+        content: t('appTutorial.slide6.content'),
         image: '/images/carousel/confident.png'
       }
     ];
